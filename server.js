6781ec870ba0e5286fc342ed7e1299b7cdd9e990
mordochi <lilynnchi@gmail.com>
2019-02-27 15:25:45 -0500

    feat: send beginning_card to clients

--- server.js
+++ server.js
@@ -10,16 +10,33 @@ const server = express().listen(PORT, () => console.log(`Listening on ${PORT}`))
 //將 express 交給 SocketServer 開啟 WebSocket 的服務
 const wss = new SocketServer({ server });
 
+
 //當 WebSocket 從外部連結時執行
 wss.on('connection', ws => {
   //連結時執行此 console 提示
   console.log('Client connected');
+  console.log(wss.clients.size)
+  
+  let i = 0;
+  wss.clients.forEach((cl) => {
+    cl.send(`players:${wss.clients.size},serverIndex:${i}`);
+    i++;
+  });
 
-  let clients = wss.clients.size
-  ws.send(`hi, there are ${clients} online.`);
+  ws.on('message', data => {
+    wss.clients.forEach((cl) => {
+      cl.send(data);
+    });
+  });
 
   //當 WebSocket 的連線關閉時執行
   ws.on('close', () => {
-      console.log('Close connected');
+    let i = 0;
+    wss.clients.forEach((cl) => {
+      cl.send(`players:${wss.clients.size},serverIndex:${i}`);
+      i++;
+    })
+    console.log('Close connected');
+    console.log(wss.clients.size)
   });
 });
